0d4dd5ebf91d71db453eeef10dc780c243169055
Jérémy Albrecht <me@jalbrecht.fr>
2024-08-10 13:49:40 +0200

    initial commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  runtimeConfig: {
-      sa: '',
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+    app: {
+      head: {
+        charset: 'utf-8',
+        viewport: 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0',
+      }
+    },
+    css: ['~/assets/css/main.css'],
+    runtimeConfig: {
+        sa: '',
     },
     postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
     },
-  },
 })
