bdcfc01444730a8691953b2b8d3cfdc1d8785929
Jérémy Albrecht <me@jalbrecht.fr>
2024-10-02 06:44:48 +0200

    add possibility to get random amount

--- app.vue
+++ app.vue
@@ -50,7 +50,8 @@ const randomPlayer = computed(() => {
 const player = ref('')
 const playerSelected = ref(false)
 
-const item = ref('')
+const item = ref({})
+const itemSelected = ref(false)
 
 const quantity = ref(1)
 
@@ -113,9 +114,9 @@ const submit = async () => {
         </a>
       </div>
     </nav>
-    <form class="max-w-sm mx-auto">
+    <form class="max-w-sm mx-auto mt-2">
       <template v-if="!completed">
-        <label for="email-address-icon" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Adhérent</label>
+        <label for="email-address-icon" class="block mb-2 text-sm font-medium text-gray-900">Adhérent</label>
         <div class="relative">
           <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
             <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -140,8 +141,8 @@ const submit = async () => {
         </div>
       </template>
 
-      <div class="grid grid-cols-3 gap-4 mt-5" v-if="playerSelected && !item && !completed">
-        <div v-on:click="item = i" v-for="i in items['data']" class="w-full p-4 bg-gray-100 dark:bg-gray-900 border border-gray-300 dark:border-gray-700 rounded-lg shadow-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors duration-300">
+      <div class="grid grid-cols-3 gap-4 mt-5" v-if="playerSelected && !itemSelected && !completed">
+        <div v-on:click="item = i; itemSelected = true;" v-for="i in items['data']" class="w-full p-4 bg-gray-100 dark:bg-gray-900 border border-gray-300 dark:border-gray-700 rounded-lg shadow-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors duration-300">
           <div class="text-center">
             <p class="text-sm font-medium text-gray-600 dark:text-gray-400">
               {{ i['Catégorie'] }}
@@ -156,7 +157,7 @@ const submit = async () => {
         </div>
       </div>
 
-      <div class="max-w-sm mx-auto mt-6" v-if="item && !completed">
+      <div class="max-w-sm mx-auto mt-6" v-if="itemSelected && !completed">
         <button class="w-full p-4 bg-gray-100 dark:bg-gray-900 border border-gray-300 dark:border-gray-700 rounded-lg shadow-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors duration-300">
           <div class="flex items-center justify-between">
             <!-- Content Container -->
@@ -174,13 +175,34 @@ const submit = async () => {
 
             <!-- Price -->
             <p class="text-sm font-semibold text-blue-600 dark:text-blue-400">
-              {{ item['Prix'] }}
+              {{ Euro.format(parseFloat(('' + item['Prix']).split(' ')[0])) }}
             </p>
           </div>
         </button>
       </div>
 
-      <template v-if="playerSelected && item && !completed">
+      <template v-if="playerSelected && itemSelected && !completed && item['Catégorie'] == 'Spécial'">
+        <div class="mt-6">
+          <label for="amount" class="block mb-2 text-sm font-medium text-gray-900">Montant</label>
+          <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+              <svg class="w-6 h-6 text-gray-800 dark:text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M19 7.11111C17.775 5.21864 15.8556 4 13.6979 4C9.99875 4 7 7.58172 7 12C7 16.4183 9.99875 20 13.6979 20C15.8556 20 17.775 18.7814 19 16.8889M5 10H14M5 14H14" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+            </div>
+            <input autocomplete="off" v-model="item['Prix']" type="number" min="0.00" max="10000.00" step="0.01" id="amount" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0.00€">
+          </div>
+        </div>
+        <div class="mt-6">
+          <label for="reason" class="block mb-2 text-sm font-medium text-gray-900">Intitulé</label>
+          <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+              <svg class="w-6 h-6 text-gray-800 dark:text-white"  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path stroke="currentColor" d="M14.635 2.217a.74.74 0 0 0-1.048 0l-10.37 10.37a.74.74 0 0 0-.217.524v5.185a.741.741 0 0 0 .217.524l2.286 2.286c-.247.6-.513.881-.722.881-.429 0-.846-.58-.982-.86l-.086-.18-1.037.493.085.18c.029.063.728 1.518 2.02 1.518a1.853 1.853 0 0 0 1.608-1.215.732.732 0 0 0 .315.077h5.185a.741.741 0 0 0 .524-.217l10.37-10.37a.74.74 0 0 0 0-1.048zM11.782 21H6.81l-.043-.043a10.076 10.076 0 0 0 .258-1.005 2.533 2.533 0 1 0-1.056-.488c-.022.079-.05.152-.066.235-.023.115-.048.216-.072.322L4 18.189v-4.97L14.11 3.106l7.783 7.782zM6 17.5A1.5 1.5 0 1 1 7.5 19c-.021 0-.04-.005-.062-.006a2.873 2.873 0 0 1 .61-.647l.16-.114-.649-.946-.165.115A4.018 4.018 0 0 0 6.39 18.5a1.489 1.489 0 0 1-.39-1zm2.542-5.792l5.922-5.922.707.707-5.922 5.923zm2.021 2.022l5.922-5.922.707.707-5.922 5.922zm2.021 2.021l5.923-5.922.707.707-5.922 5.922z"></path><path fill="none" d="M0 0h24v24H0z"></path></g></svg>
+            </div>
+            <input autocomplete="off" v-model="item['Item']" type="text" id="reason" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0.00€">
+          </div>
+        </div>
+      </template>
+
+      <template v-if="playerSelected && itemSelected && !completed">
         <div class="relative flex items-center max-w-sm mx-auto mt-6" >
           <button v-on:click="quantity--" type="button" id="decrement-button" data-input-counter-decrement="quantity-input" class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-12 h-32 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
             <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
@@ -196,12 +218,12 @@ const submit = async () => {
         </div>
       </template>
 
-      <template v-if="playerSelected && item && quantity > 0 && !completed">
+      <template v-if="playerSelected && itemSelected && quantity > 0 && !completed">
         <div class="fixed inset-x-0 bottom-0 p-4">
           <div v-on:click="submit()" class="w-full bg-blue-600 text-white text-lg font-semibold py-4 rounded-lg shadow-lg flex items-center justify-center space-x-4 hover:bg-blue-700 dark:hover:bg-blue-500 transition-colors duration-300">
             <!-- Price -->
             <span class=" bg-white text-blue-600 text-lg font-bold py-2 px-4 rounded-md">
-              {{ Euro.format(quantity * parseFloat(item['Prix'].split(' ')[0]))}}
+              {{ Euro.format(quantity * parseFloat(('' + item['Prix']).split(' ')[0]))}}
             </span>
             <!-- Button Text -->
             <span v-if="!loading">Enregistrer la dette</span>
